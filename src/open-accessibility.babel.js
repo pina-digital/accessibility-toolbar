204d21f78317b7e98357c3f69424ec1f41e0c664
Lior Hasson <liorhasson@gmail.com>
2020-12-17 19:11:43 +0200

    v.2.31

--- src/open-accessibility.babel.js
+++ src/open-accessibility.babel.js
@@ -1146,6 +1146,7 @@
           .addClass("hide-images");
         $("img[src]").addClass("hide-images");
         $("div[background-image]").addClass("hide-images");
+        $("div").addClass("hide-images");
 
         $("div[src]").addClass("hide-images");
         $("iframe[src]").addClass("hide-images");
